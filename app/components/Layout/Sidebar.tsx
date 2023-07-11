16c0c264c6fdf44f8621ce1ce0009cb6d2431eee
Sami Zakir Ahmed <54m1z4k@gmail.com>
2023-07-11 20:22:29 +0100

    Implemented sidebar

---
+++ app/components/Layout/Sidebar.tsx
@@ -0,0 +1,44 @@
+"use client";
+
+import React from "react";
+import { BsHouseFill, BsBellFill } from "react-icons/bs";
+import { FaUser } from "react-icons/fa";
+import SidebarLogo from "./SidebarLogo";
+import SidebarItem from "./SidebarItem";
+import SidebarTweetButton from "./SidebarTweetButton ";
+
+export default function Sidebar() {
+  const items = [
+    {
+      label: "Home",
+      href: "",
+      icon: BsHouseFill,
+    },
+    {
+      label: "Notifications",
+      href: "/notifications",
+      icon: BsBellFill,
+    },
+    {
+      label: "Profile",
+      href: "/users/132",
+      icon: FaUser,
+    },
+  ];
+
+  return (
+    <div className="col-span-1 h-full pr-4 md:pr-6">
+      <div className="flex flex-col items-end">
+        <div className="space-y-2 lg:w-[230px]">
+          <SidebarLogo />
+
+          {items.map((item) => (
+            <SidebarItem key={item.href} href={item.href} icon={item.icon} label={item.label} />
+          ))}
+
+          <SidebarTweetButton />
+        </div>
+      </div>
+    </div>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,10 @@
 import React from "react";
+import Layout from "./components/Layout/Layout";
 
 export default function Page() {
-  return <div className="text-3xl text-sky-500">Twitter</div>;
+  return (
+    <Layout>
+      <div className="text-3xl text-sky-500">Twitter</div>
+    </Layout>
+  );
 }

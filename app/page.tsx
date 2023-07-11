5e26986711b4a057e0dd227cd4bf79defe00321c
Sami Zakir Ahmed <54m1z4k@gmail.com>
2023-07-11 20:52:14 +0100

    Created header in home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import Layout from "./components/Layout/Layout";
+import Header from "./components/Header";
 
 export default function Page() {
   return (
     <Layout>
-      <div className="pl-4 py-4 text-2xl text-white">Home</div>
+      <Header label="Home" />
     </Layout>
   );
 }

fe5b3170d7f605c68359449169b3022a34d71c63
Sami Zakir Ahmed <54m1z4k@gmail.com>
2023-07-15 16:20:04 +0100

    Added React Toast to project

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,25 @@
+"use client";
+
+import { useSession } from "next-auth/react";
+import { redirect } from "next/navigation";
 import React from "react";
 import Layout from "./components/Layout/Layout";
 import Header from "./components/Header";
 import Register from "./components/Modal/Register";
 import Login from "./components/Modal/Login";
+import { Toaster } from "react-hot-toast";
 
-export default function Page() {
+export default function Home() {
+  const { data: session } = useSession({
+    required: true,
+    onUnauthenticated() {
+      // redirect("/");
+    },
+  });
+
   return (
     <>
+      <Toaster />
       <Login />
       <Register />
 

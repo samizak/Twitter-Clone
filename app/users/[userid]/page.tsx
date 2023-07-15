cfaafd55109870795d8ae5490fa9a309e3bf4495
Sami Zakir Ahmed <54m1z4k@gmail.com>
2023-07-15 17:14:15 +0100

    Created user profile

---
+++ app/users/[userid]/page.tsx
@@ -0,0 +1,33 @@
+"use client";
+
+import { ClipLoader } from "react-spinners";
+import useUser from "@/hooks/useUser";
+
+// import PostFeed from "@/components/posts/PostFeed";
+import Header from "@/app/components/Header";
+import UserHero from "@/app/components/Users/UserHero";
+import UserBio from "@/app/components/Users/UserBio";
+
+const UserView = ({ params }: any) => {
+  const { userid: userId } = params;
+  const { data: fetchedUser, isLoading } = useUser(userId as string);
+
+  if (isLoading || !fetchedUser) {
+    return (
+      <div className="flex items-center justify-center h-full">
+        <ClipLoader color="lightblue" size={80} />
+      </div>
+    );
+  }
+
+  return (
+    <>
+      <Header showBackArrow label={fetchedUser?.name} />
+      <UserHero userId={userId as string} />
+      <UserBio userId={userId as string} />
+      {/* <PostFeed userId={userId as string} /> */}
+    </>
+  );
+};
+
+export default UserView;

3afb93008be3492a93ffd21222a59c2906622054
Sami Zakir Ahmed <54m1z4k@gmail.com>
2023-07-16 01:19:38 +0100

    minor bug fixes

--- app/components/Layout/Layout.tsx
+++ app/components/Layout/Layout.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import React from "react";
 import Sidebar from "./Sidebar";
 import FollowBar from "./FollowBar";
--- app/components/Modal/EditModal.tsx
+++ app/components/Modal/EditModal.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import axios from "axios";
 import { useCallback, useEffect, useState } from "react";
 import { toast } from "react-hot-toast";
